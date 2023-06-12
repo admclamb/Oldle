f829246a7b8252c3c69adecf90c2b9dfc6677f84
Anthony Mclamb <adylanmclamb@yahoo.com>
2023-06-12 00:43:27 -0400

    Update service and repo

--- application/application/src/main/java/com/oldle/application/common/classes/Service.java
+++ application/application/src/main/java/com/oldle/application/common/classes/Service.java
@@ -1,15 +1,20 @@
-package com.oldle.application.common;
+package com.oldle.application.common.classes;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public abstract class Service<T>{
     private MongoRepository<T, String> repository;
 
-    Service(MongoRepository<T, String> repository) {
+    public Service(MongoRepository<T, String> repository) {
         this.repository = repository;
     }
 
     public T findOneById(String id) {
         return repository.findById(id).get();
+    }
+
+
+    public MongoRepository<T, String> getRepository() {
+        return this.repository;
     }
 }
--- application/application/src/main/java/com/oldle/application/picture/PictureRepository.java
+++ application/application/src/main/java/com/oldle/application/picture/PictureRepository.java
@@ -1,10 +1,6 @@
 package com.oldle.application.picture;
 
-import java.util.List;
-
 import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.oldle.application.common.primitives.Id;
 
 public interface PictureRepository extends MongoRepository<Picture, String> {
 }
