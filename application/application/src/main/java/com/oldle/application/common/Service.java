ee68ab50833b4974b1407d0eabbd8fae60f3c75e
Anthony Mclamb <adylanmclamb@yahoo.com>
2023-06-11 21:18:32 -0400

    Use constructor injection

--- application/application/src/main/java/com/oldle/application/common/Service.java
+++ application/application/src/main/java/com/oldle/application/common/Service.java
@@ -1,12 +1,13 @@
 package com.oldle.application.common;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public abstract class Service<T>{
-    
-    @Autowired
     private MongoRepository<T, String> repository;
+
+    Service(MongoRepository<T, String> repository) {
+        this.repository = repository;
+    }
 
     public T findOneById(String id) {
         return repository.findById(id).get();
