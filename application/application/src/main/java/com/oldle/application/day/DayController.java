633199690ff309360f7fe488932b9aefd2d21fdc
Anthony Mclamb <adylanmclamb@yahoo.com>
2023-06-11 21:09:47 -0400

    Update service class

--- application/application/src/main/java/com/oldle/application/day/DayController.java
+++ application/application/src/main/java/com/oldle/application/day/DayController.java
@@ -11,8 +11,9 @@ public class DayController {
     private DayService dayService;
     
     @GetMapping("/day")
-    public String getDay(@RequestParam("id") String id) {
+    public Day getDay(@RequestParam("id") String id) {
         Day day = dayService.findOneById(id);
+        return day;
     }
     
 }
