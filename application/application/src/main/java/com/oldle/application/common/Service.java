4e681587ec5485974f12ccb614449d3fe3fa36c5
Anthony Mclamb <adylanmclamb@yahoo.com>
2023-06-11 21:09:38 -0400

    Update base Service class

--- application/application/src/main/java/com/oldle/application/common/Service.java
+++ application/application/src/main/java/com/oldle/application/common/Service.java
@@ -1,13 +1,14 @@
 package com.oldle.application.common;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public abstract class Service<T>{
     
+    @Autowired
     private MongoRepository<T, String> repository;
 
     public T findOneById(String id) {
-        return repository.findOne(id);
+        return repository.findById(id).get();
     }
-
 }
