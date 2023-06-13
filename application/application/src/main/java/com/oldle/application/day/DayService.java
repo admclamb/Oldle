c5715978ff5b258c3259f0bb994c3ad2276ab0f5
Anthony Mclamb <adylanmclamb@yahoo.com>
2023-06-12 23:54:32 -0400

    Fix service

--- application/application/src/main/java/com/oldle/application/day/DayService.java
+++ application/application/src/main/java/com/oldle/application/day/DayService.java
@@ -2,15 +2,15 @@ package com.oldle.application.day;
 
 import java.util.Map;
 
+import org.springframework.stereotype.Service;
 
-import com.oldle.application.common.classes.repository.PaginationRepository;
 import com.oldle.application.common.classes.service.PaginationService;
 
+
+
+@Service
 public class DayService extends PaginationService<Day> {
 
-    public DayService(PaginationRepository<Day, String> repository) {
-        super(repository);
-    }
     public Map<String, Object> listDays(int page, int size) {
         return this.findAll(page, size);
     }
