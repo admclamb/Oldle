cfe074ff30040f5399fd25b17b2f068941d7f2f1
Anthony Mclamb <adylanmclamb@yahoo.com>
2023-06-15 16:42:37 -0400

    Fix repo

--- application/application/src/main/java/com/oldle/application/day/DayRepository.java
+++ application/application/src/main/java/com/oldle/application/day/DayRepository.java
@@ -1,8 +1,15 @@
 package com.oldle.application.day;
 
 
-import org.springframework.data.mongodb.repository.MongoRepository;
+import java.util.List;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface DayRepository extends MongoRepository<Day, String>{
     
+    Page<List<Day>> findAllPageable(Pageable pageable);
 }
