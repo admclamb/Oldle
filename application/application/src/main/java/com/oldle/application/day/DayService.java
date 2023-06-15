00a508c9bcc9572f59edef3e1cdc7fc2a6fa4f57
Anthony Mclamb <adylanmclamb@yahoo.com>
2023-06-15 16:42:43 -0400

    Update repo

--- application/application/src/main/java/com/oldle/application/day/DayService.java
+++ application/application/src/main/java/com/oldle/application/day/DayService.java
@@ -1,17 +1,37 @@
 package com.oldle.application.day;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import com.oldle.application.common.classes.service.PaginationService;
-
-
-
 @Service
-public class DayService extends PaginationService<Day> {
+public class DayService {
+
+    private DayRepository repository;
+
+    public DayService(DayRepository repository) {
+        
+    }
+
+    public Page<List<Day>> findAll(int page, int size){
+        Pageable paging = PageRequest.of(page, size);
+        
+        Page<List<Day>> pages = this.repository.findAllPageable(paging);
+        return pages;
+    }
 
     public Map<String, Object> listDays(int page, int size) {
-        return this.findAll(page, size);
+        Page<List<Day>> pageOfDays = this.findAll(page, size);
+        Map<String, Object> response = new HashMap<>();
+        response.put("items", pageOfDays.getContent());
+        response.put("currentPage", pageOfDays.getNumber());
+        response.put("totalItems", pageOfDays.getTotalElements());
+        response.put("totalPages",pageOfDays.getTotalPages());
+        return response;
     }
 }
