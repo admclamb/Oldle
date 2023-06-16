04ac17e99e8e1902590434bd5ae3aef589e59368
Anthony Mclamb <adylanmclamb@yahoo.com>
2023-06-15 21:28:59 -0400

    Fix repo

--- application/application/src/main/java/com/oldle/application/day/DayRepository.java
+++ application/application/src/main/java/com/oldle/application/day/DayRepository.java
@@ -1,9 +1,6 @@
 package com.oldle.application.day;
 
 import org.springframework.data.domain.Pageable;
-
-import java.util.List;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
