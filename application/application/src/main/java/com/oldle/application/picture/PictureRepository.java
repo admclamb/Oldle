4141ea8c2ad7c883e2bba57be76de571acffedec
Anthony Mclamb <adylanmclamb@yahoo.com>
2023-06-15 21:25:38 -0400

    Fix day feature

--- application/application/src/main/java/com/oldle/application/picture/PictureRepository.java
+++ application/application/src/main/java/com/oldle/application/picture/PictureRepository.java
@@ -1,6 +1,8 @@
 package com.oldle.application.picture;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PictureRepository extends MongoRepository<Picture, String> {
 }
